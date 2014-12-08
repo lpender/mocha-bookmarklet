cf81634314f22a1f23e8d1d71b88fb47e1018792
Lee Pender <lpender@gmail.com>
2014-12-08 14:42:28 -0500

    Assertions working

--- testmarklet.js
+++ testmarklet.js
@@ -8,17 +8,16 @@
     scriptTimeout: 3000
   };
 
-
-  function assert(expr, msg) {
-    if (!expr) throw new Error(msg || 'failed');
-  }
-
   var callback = function() {
-    debugger;
     mocha.setup('bdd');
+
     loadScript("./test.js", function() {
       var runner = mocha.run();
     });
+  }
+
+  window.assert = function assert(expr, msg) {
+    if (!expr) throw new Error(msg || 'failed');
   }
 
   body = document.getElementsByTagName("body")[0]
@@ -31,24 +30,24 @@
   loadStylesheet(config.expectCss)
   loadScript(config.expectJs, callback);
 
-function loadStylesheet(url) {
-  var link = document.createElement("link");
-  link.href = url;
-  link.rel = "stylesheet"
+  function loadStylesheet(url) {
+    var link = document.createElement("link");
+    link.href = url;
+    link.rel = "stylesheet"
     link.type = "text/css"
 
     document.getElementsByTagName("head")[0].appendChild(link);
-}
+  }
 
-function loadScript(url, callback) {
-  var script = document.createElement("script")
+  function loadScript(url, callback) {
+    var script = document.createElement("script")
     script.src = url;
-  script.type = "text/javascript"
+    script.type = "text/javascript"
 
     script.onload = function () {
       callback();
     }
 
-  document.getElementsByTagName("head")[0].appendChild(script);
-}
+    document.getElementsByTagName("head")[0].appendChild(script);
+  }
 })()
