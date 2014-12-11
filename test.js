6eb5a8f6afd893c0f6f5e3a42c52ad2ac1b2b744
Lee Pender <lpender@gmail.com>
2014-12-11 02:18:33 -0500

    Replace 'querySelectorAll' with custom 'find' method

--- test.js
+++ test.js
@@ -1,46 +1,45 @@
 describe("Front End Challenge 2 - Introduction to HTML & CSS", function() {
   describe("Activities 1-7", function() {
     it("has an h1 heading with content Heading 1", function () {
-      element = document.querySelectorAll('h1')[0];
-      expect(element.innerHTML).to.contain("Heading 1");
+      var elements = find("h1");
+      expect(elements).to.have.length(1);
+      expect(elements[0].innerHTML).to.contain("Heading 1");
     });
     it("has an h2 heading", function () {
-      element = document.querySelectorAll('h2');
+      var element = find("h2");
       expect(element.length).to.least(1);
     });
     it("has an h3 heading", function () {
-      element = document.querySelectorAll('h3');
+      var element = find("h3");
       expect(element.length).least(1);
     });
     it("has an h4 heading", function () {
-      element = document.querySelectorAll('h4');
+      var element = find("h4");
       expect(element.length).least(1);
     });
     it("has an h5 heading", function () {
-      element = document.querySelectorAll('h5');
+      var element = find("h5");
       expect(element.length).least(1);
     });
     it("has an h6 heading", function () {
-      element = document.querySelectorAll('h6');
+      var element = find("h6");
       expect(element.length).least(1);
     });
     it("has 4 divs with p in them", function() {
-      elements = document.querySelectorAll('p');
+      var elements = find("p");
       expect(elements).to.have.length(4);
     });
     it("has a ul", function() {
-      elements = document.querySelectorAll("ul")[0];
-      expect(element.length).least(1)
+      var elements = find("ul");
+      expect(elements.length).least(1);
     });
     it("has an ol", function() {
-      elements = document.querySelectorAll("ol")[0];
-      expect(element.length).least(1)
+      var elements = find("ol");
+      expect(elements.length).least(1);
     });
     it("has an element with emphasis", function() {
-      elements = document.querySelectorAll("strong")[0];
-      expect(element.length).least(1)
+      var elements = find("strong");
+      expect(elements.length).least(1);
     });
-
-
   });
 });
